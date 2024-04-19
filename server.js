27b956da900e161cdd7807fe561f70c0ed60191d
ianhara <ianhara97@gmail.com>
2024-04-19 08:44:35 -0400

    started working on server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
