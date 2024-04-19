b633fde3ec5283ed90302d0fc08e7ac93e06ce36
ianhara <ianhara97@gmail.com>
2024-04-19 08:59:08 -0400

    setting up route for home page

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get(('/'),(req, res)=>{
+res.sendFile(path.join(__dirname, "./public/assets/index.html"))
+})
