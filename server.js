14d1f0551d757d40d17d1b2eda5dd842b1dce9bd
ianhara <ianhara97@gmail.com>
2024-04-19 13:40:17 -0400

    adding routes for note taking

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+
+const notePath = path.join(__dirname, 'db.json')
+
+
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = 3000;
 
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
-app.get(('/'),(req, res)=>{
-res.sendFile(path.join(__dirname, "./public/assets/index.html"))
+app.get("/notes", (req, res)=>{
+ res.sendFile(path.join(__dirname,"./public/notes.html"))
+});
+
+app.get('/api/notes',(req, res)=>{
+    console.log("in the /api/notes route")
+    //fs.readFile to 
+    fs.readFile(notePath, 'utf8',(err,data)=>{
+res.send(data)
+    })
+    //send results 
+})
+
+app.get(('*'),(req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+    })
+
+
+
+app.listen(PORT,function (){
+    console.log(`Server Runnning on PORT http://localhost:${PORT}`)
 })
