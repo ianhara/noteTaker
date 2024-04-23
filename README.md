a249bd7502e047ce97055eb64917dfd8cb1e3090
ianhara <ianhara97@gmail.com>
2024-04-22 20:20:08 -0400

    completed routes

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Note Taker Starter Code
+# NoteTaker
+
+  web app for taking notes.
+
+## Features
+
+- View existing notes
+- Add new notes
+- Delete notes
+
+## Technologies Used
+
+- Node.js
+- Express.js
+- HTML
+- CSS
+- JavaScript
+
+## Installation
+
+1. Clone the repository:
+
+git clone https://github.com/ianhara/noteTaker.git
+
+markdown
+Copy code
+
+2. Install dependencies:
+
+3. Start the server:
+
+4. Open your web browser and navigate to [http://localhost:3000](http://localhost:3000) to use the application.
+
+## Usage
+
+- To view existing notes, navigate to the homepage.
+- To add a new note, click on the "New Note" button and enter the note title and content in the provided fields. Then, click the "Save" button.
+- To delete a note, click on the trash icon next to the note you want to delete.
+
+## contribution 
+-tutor from canvas
+
+
+
+
--- server.js
+++ server.js
@@ -1,36 +1,53 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-const notePath = path.join(__dirname, 'db.json')
-
+const notePath = path.join(__dirname, "db.json");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get("/notes", (req, res)=>{
- res.sendFile(path.join(__dirname,"./public/notes.html"))
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get('/api/notes',(req, res)=>{
-    console.log("in the /api/notes route")
-    //fs.readFile to 
-    fs.readFile(notePath, 'utf8',(err,data)=>{
-res.send(data)
-    })
-    //send results 
-})
+app.get("/api/notes", (req, res) => {
+    console.log("in the /api/notes route");
+    //fs.readFile to
+    fs.readFile(notePath, "utf8", (err, data) => {
+      res.send(data);
+    });
+  });
+  
 
-app.get(('*'),(req, res)=>{
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-    })
+app.post("/api/notes", (req, res) => {
+    //not sure if this right 
+  const { title, text } = req.body;
+  const note = { title, text, id: uuidv4() };
+  fs.readFile(notePath, "utf8", (err, data) => {
+    if (!err) {
+      const notes = JSON.parse(data);
+      notes.push(note);
+
+      fs.writeFile(notePath, JSON.stringify(notes), (err) => {
+        if (!err) {
+          res.json(note);
+        }
+      });
+    }
+  });
+});
 
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.listen(PORT,function (){
-    console.log(`Server Runnning on PORT http://localhost:${PORT}`)
-})
+app.listen(PORT, function () {
+  console.log(`Server Runnning on PORT http://localhost:${PORT}`);
+});
